1ab5a92862119067f0b6b7d14b314dc3f513c143
Benjamin Clauss <benjaminclauss@protonmail.com>
2021-03-21 13:08:53 -0400

    Get API endpoints

---
+++ get_api_endpoints.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+from workflow import Workflow, web
+
+def main(wf):
+	url = 'https://swapi.dev/api/'
+	params = dict(format='json')
+	r = web.get(url, params)
+	
+	r.raise_for_status()
+	result = r.json()
+	
+	# Loop through the returned endpoint and add an item for each to the list of results for Alfred.
+	for endpoint in result:
+		subtitle = "Get " + endpoint + "data"
+		wf.add_item(title=endpoint, subtitle=subtitle, arg=endpoint, valid=True)
+	
+	wf.send_feedback()
+
+if __name__ == "__main__":
+	wf = Workflow()
+	sys.exit(wf.run(main))
